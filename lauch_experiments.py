d03aa73e318ffc39feb18dd20a604b1507e7b93c
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-03-09 16:10:39 +0100

    Create the exp launcher

---
+++ lauch_experiments.py
@@ -0,0 +1,14 @@
+import subprocess
+from pathlib import Path
+
+def lauch_experiment(path):
+    command = f"sbatch {path}"
+    try:
+        subprocess.check_call(command, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"command {e.cmd} exited with error code {e.returncode}.")
+
+EXP_DIR = Path("./experiments")
+for exp in EXP_DIR.iterdir():
+    for script in exp.glob("*.sh"):
+        lauch_experiment(str(script))
