cc00d54ea742c2d0bc3bff04ed9559feae218f70
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-03-23 20:15:29 +0100

    Simplify sisnr computation

--- kaituoxu/pit_criterion.py
+++ kaituoxu/pit_criterion.py
@@ -43,6 +43,41 @@ def cal_loss(source, estimate_source, source_lengths):
     reorder_estimate_source = reorder_source(estimate_source, perms, max_snr_idx)
     return loss, max_snr, estimate_source, reorder_estimate_source
 
+def sisnr(source, estimate_source):
+    """
+    Args:
+        source: [B, C, T], B is batch size
+        estimate_source: [B, C, T]
+        source_lengths: [B]
+    """
+    B, C, T = source.size()
+    # mask padding position along T
+    
+    # Step 1. Zero-mean norm
+    mean_target = torch.mean(source, dim=2, keepdim=True)
+    mean_estimate = torch.mean(estimate_source, dim=2, keepdim=True)
+    zero_mean_target = source - mean_target
+    zero_mean_estimate = estimate_source - mean_estimate
+
+    # Step 2. SI-SNR with PIT
+    # reshape to use broadcast
+    s_target = torch.unsqueeze(zero_mean_target, dim=1)  # [B, 1, C, T]
+    s_estimate = torch.unsqueeze(zero_mean_estimate, dim=2)  # [B, C, 1, T]
+    
+    # s_target = <s', s>s / ||s||^2
+    pair_wise_dot = torch.sum(s_estimate * s_target, dim=3, keepdim=True)  # [B, C, C, 1]
+    s_target_energy = torch.sum(s_target ** 2, dim=3, keepdim=True) + EPS  # [B, 1, C, 1]
+    pair_wise_proj = pair_wise_dot * s_target / s_target_energy  # [B, C, C, T]
+    
+    # e_noise = s' - s_target
+    e_noise = s_estimate - pair_wise_proj  # [B, C, C, T]
+    
+    # SI-SNR = 10 * log_10(||s_target||^2 / ||e_noise||^2)
+    pair_wise_si_snr = torch.sum(pair_wise_proj ** 2, dim=3) / (torch.sum(e_noise ** 2, dim=3) + EPS)
+    pair_wise_si_snr = - 10 * torch.log10(pair_wise_si_snr + EPS)  # [B, C, C]
+    
+    return pair_wise_si_snr
+
 
 def cal_si_snr_with_pit(source, estimate_source, source_lengths):
     """Calculate SI-SNR with PIT training.
@@ -118,7 +153,7 @@ def reorder_source(source, perms, max_snr_idx):
     return reorder_source
 
 
-def get_mask(source, source_lengths):
+def get_mask(source, source_lengths=None):
     """
     Args:
         source: [B, C, T]
@@ -128,8 +163,9 @@ def get_mask(source, source_lengths):
     """
     B, _, T = source.size()
     mask = source.new_ones((B, 1, T))
-    for i in range(B):
-        mask[i, :, int(source_lengths[i].item()):] = 0
+    if source_lengths is not None:
+        for i in range(B):
+            mask[i, :, int(source_lengths[i].item()):] = 0
     return mask
 
 
